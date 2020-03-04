720755b6f093437e6e3ba9468770e1f627d49c6c
AllisonRMiller <60373159+AllisonRMiller@users.noreply.github.com>
2020-03-03 22:12:40 -0500

    functions added
    basic functions added

---
+++ main.js
@@ -0,0 +1,47 @@
+var zip = document.getElementById("inlineFormInputZip").value;
+//location name
+var loc = "";
+//temp in f
+var f = "";
+//current conditions
+var cur = "";
+//icon
+var ic = "";
+//PLAY FETCH
+
+fetch(api.openweathermap.org/data/2.5/weather?zip={zip},{us}&appid=b9cb3cec77c14b5037dec0def981f206).then(getstuff(response))
+
+onclick
+
+function getstuff(response){
+    gettemp(response.main);
+    getloc(response.name);
+    getcur(response.weather);
+    getic(response.weather);
+}
+
+function getloc(name){
+    loc = name;
+    document.getElementById("cityname").innerHTML = loc;
+}
+
+function gettemp(main){
+    f = eval((main.temp - 273.15) * 9/5 + 32); 
+    document.getElementById("temp").innerHTML = f;
+}
+
+function getcur(weather){
+    cur = weather.main + " : "+ weather.description;
+    document.getElementById("cond").innerHTML = cur;
+}
+
+function getic(weather){
+    ic = weather.icon;
+    document.getElementById("season").innerHTML = <img src= "http://openweathermap.org/img/w/" + ic +".png">
+}
+
+// function validate(){
+//     if (zip.length == 5){//perform call}
+//     else {document.getElementById("zipalert").setAttribute("class","alert alert-warning")
+
+// }
